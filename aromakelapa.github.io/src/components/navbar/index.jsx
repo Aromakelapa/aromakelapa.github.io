602e7d75ed63f2d32a7a17f2ad01eb0d7cd0397e
Gilang Muharom <gmrom1404@gmail.com>
2023-04-29 09:19:37 +0700

    Update

---
+++ aromakelapa.github.io/src/components/navbar/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className='my-navbar'>
+        <div className='container'>
+          <h6>Hello</h6>
+        </div>
+      </nav>
+    );
+  };
+};
+
+export default Navbar;
